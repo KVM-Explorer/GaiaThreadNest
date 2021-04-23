a2baa694543b884753e44366dd09d64bdec277eb
lightning <wfcyywh1225@126.com>
2021-04-23 23:27:15 +0800

    Make a unity test and define Worker

--- GaiaThreadNest/Worker.hpp
+++ GaiaThreadNest/Worker.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "Timestamp.hpp"
+#include <functional>
+#include <atomic>
+#include <thread>
+#include <memory>
+#include <future>
 
 namespace Gaia::ThreadNest
 {
     class Worker
     {
-        friend class Nest;
-
+        using Task = std::function<void()> ;
     private:
         /**
          * @brief The timestamp used to control its life span.
@@ -17,5 +21,24 @@ namespace Gaia::ThreadNest
          *  timestamp to know whether nest thread ()
          */
         Timestamp LifeTimestamp;
+        Task Work;                                //work is what this worker should do
+        std::unique_ptr<std::thread> Thread;     //thread point
+        std::atomic<bool> IsAlive;               //control this thread is alive if false this thread should be killed
+        std::promise<bool> IsStart;              //Async control thread start up to work
+    public:
+        /**
+         * @brief stop this worker's work
+         * @details Nest can throughout this to control Worker
+         *          After use this Worker's work is stopped and relevant sources are released
+         */
+        void Stop();
+
+        /**
+         * @brief Nest use this to start up Worker
+         * @details thread has been built, but it don't work immediately because of IsStart
+         *          thus it can archive async operate
+         */
+        void Execute();
+
     };
 }
---
+++ UnityTest/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by lightning on 2021/4/23.
+//
+
+#include "../GaiaThreadNest/Nest.hpp"
+#include <iostream>
+
+void function1()
+{
+    std::cout<<"Value:"<<1<<std::endl;
+}
+
+void function2()
+{
+    std::cout<<"Unity:"<<2<<std::endl;
+}
+
+void function3()
+{
+    std::cout<<"Unity:"<<3<<std::endl;
+}
