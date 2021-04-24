704720c57631790ce5a74f1c916d682b3b643d0e
lightning <wfcyywh1225@126.com>
2021-04-24 11:11:07 +0800

    archive Worker Execute() and Stop() method

--- GaiaThreadNest/Worker.cpp
+++ GaiaThreadNest/Worker.cpp
@@ -2,5 +2,22 @@
 
 namespace Gaia::ThreadNest
 {
+    void Worker::Execute()
+    {
+        IsStart.set_value(true);
+    }
 
+    void Worker::Stop()
+    {
+        auto worker_state = IsStart.get_future();
+        // after using std::future get() method, the shared state between Promise and future will be released
+        if(worker_state.valid()){
+            Execute();
+        }
+        IsAlive= false;
+        if(Thread->joinable())
+        {
+            Thread->join();
+        }
+    }
 }
